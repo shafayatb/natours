20077650ab061a5db58c8398c521b58e4e0169b2
Shafayat <mindfreakicup33@gmail.com>
2020-03-23 01:00:07 +0600

    Handling DELETE Request

--- app.js
+++ app.js
@@ -87,6 +87,23 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  //if (id >= tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid Id'
+    });
+  }
+  
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
