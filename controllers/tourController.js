680700dacbebace3f968e9ae92b493fb0754c55a
Shafayat <mindfreakicup33@gmail.com>
2020-04-04 08:26:40 +0600

    Param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,18 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  const id = val * 1;
+  const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id'
+    });
+  }
+  next();
+}
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -17,17 +29,8 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   console.log(req.params);
-
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
-  //if (id >= tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid Id'
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -37,7 +40,6 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  //console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -59,16 +61,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  //if (id >= tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid Id'
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -78,16 +70,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  //if (id >= tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid Id'
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null
