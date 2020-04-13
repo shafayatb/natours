71bfca3b6ba36f6e67912d309851079ee22ee8fc
Shafayat <mindfreakicup33@gmail.com>
2020-04-13 20:27:52 +0600

    Data validation with built-in validators.

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,10 @@ const tourSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
-      unique: true
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal 40 characters'],
+      minlength: [10, 'A tour name must have more or equal 10 characters']
     },
     slug: String,
     duration: {
@@ -19,11 +22,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be above 5.0']
     },
     ratingsQuantity: {
       type: Number,
