14336f333175b9d4603362ffac5efb2c37042a7b
Shafayat <mindfreakicup33@gmail.com>
2020-04-13 20:03:15 +0600

    Aggragation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -89,7 +89,11 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took: ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  next();
+});
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
 
