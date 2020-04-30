e328b3fa94cd9c31e3415faf4024f68780184596
Shafayat <mindfreakicup33@gmail.com>
2020-04-30 20:08:56 +0600

    User model and creating new user

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm password'],
+    minlength: 8
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
