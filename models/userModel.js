538092315b1d1a7c7a9a686c0066450f35a7ffdd
Shafayat <mindfreakicup33@gmail.com>
2020-05-26 20:28:13 +0600

    Managing password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs'); 
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,8 +23,23 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm password'],
-    minlength: 8
+    minlength: 8,
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same!'
+    }
   }
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
