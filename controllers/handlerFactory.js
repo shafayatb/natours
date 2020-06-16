377c96c578d380eaad781024f38383b06e8a4e39
Shafayat <mindfreakicup33@gmail.com>
2020-06-16 20:13:40 +0600

    Factory Function for create and update

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,3 @@
-const { Model } = require('mongoose');
-
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -14,5 +12,36 @@ exports.deleteOne = (Model) =>
     res.status(204).json({
       status: 'success',
       data: null
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
     });
   });
