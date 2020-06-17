b0b088baa9251c5c9b507177946b58300f09146a
Shafayat <mindfreakicup33@gmail.com>
2020-06-17 16:55:25 +0600

    Read performance improvement with indexiing

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,8 +21,10 @@ mongoose
   .then(() => console.log('DB connection Succesful'));
 
 // READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8')
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
 );
 
 //IMPORT DATA INTO DB
@@ -28,6 +32,8 @@ const tours = JSON.parse(
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Successfully imported data.');
   } catch (err) {
     console.log(err);
@@ -39,6 +45,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Successfully deleted data.');
   } catch (err) {
     console.log(err);
